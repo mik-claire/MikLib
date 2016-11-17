9bd8271c8b10b7da331215644f849f2b9e6592c1
西村 波宏 <0069@if-domain.local>
2016-11-17 20:19:01 +0900

    Implemented var_dump and ComparableModel class.

---
+++ MikLib/Model/ComparableModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace MikLib.Model
+{
+	public class ComparableModel
+	{
+		public bool IsSameData(ComparableModel target, params string[] ignoreProperyNames)
+		{
+			Type type = this.GetType();
+			PropertyInfo[] properties = type.GetProperties();
+			Console.WriteLine("type: {0}", type.Name);
+
+			foreach (PropertyInfo pi in properties)
+			{
+				Console.WriteLine("  property: {0}", pi.Name);
+
+				bool hit = false;
+				foreach (string name in ignoreProperyNames)
+				{
+					if (name != pi.Name)
+					{
+						continue;
+					}
+
+					hit = true;
+					break;
+				}
+				if (hit)
+				{
+					Console.WriteLine("    continue.");
+					continue;
+				}
+
+				object currentVaule = pi.GetValue(this, null);
+				object targetValue = pi.GetValue(target, null);
+
+				Console.WriteLine("    current: {0}", currentVaule);
+				Console.WriteLine("    target : {0}", targetValue);
+
+				if (currentVaule.Equals(targetValue))
+				{
+					continue;
+				}
+
+				Console.WriteLine();
+				return false;
+			}
+
+			Console.WriteLine();
+			return true;
+		}
+	}
+}
